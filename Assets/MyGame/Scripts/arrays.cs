160164a788274ac25dcbafafee1beb7f627ed037
pklacher <pia.lacher04@htl-salzburg.ac.at>
2021-03-16 23:16:49 +0100

    got it right i think

--- Assets/MyGame/Scripts/arrays.cs
+++ Assets/MyGame/Scripts/arrays.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 public class arrays : MonoBehaviour
 {
     int[] myIntArray = { 23, 34, 45 };
-    string[] myStringArray = { "Number One", "Number Two", "Number Three" };
+    string[] myStringArray = { "One", "Two", "Three" };
     float[] myFloatArray = { 1.234f, 2.345f, 3.456f };
     bool[] myBoolArray = { false, true, false };
 
-    int myIntArrayOne = 23;
-    int myIntArrayTwo = 34;
-    int myIntArrayThree = 45;
+    int myIntOne = 23;
+    int myIntTwo = 34;
+    int myIntThree = 45;
+
+    string myStringOne = "One";
+    string myStringTwo = "Two";
+    string myStringThree = "Three";
 
     bool myBoolOne = false;
     bool myBoolTwo = true;
     bool myBoolThree = false;
 
-    public GameObject square;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(myStringArray);
-    }
+        for(int i = 0; i < myStringArray.Length; i++)
+        {
+            Debug.Log(myStringArray[i]);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        Debug.Log(myStringOne);
+        Debug.Log(myStringTwo);
+        Debug.Log(myStringThree);
 
+        for(int i = 0; i < myIntArray.Length; i++)
+        {
+            Debug.Log(myIntArray[i]);
+        }
+
+        Debug.Log(myIntOne);
+        Debug.Log(myIntTwo);
+        Debug.Log(myIntThree);
+
+        for (int i = 0; i < myBoolArray.Length; i++)
+        {
+            Debug.Log(myBoolArray[i]);
+        }
+
+        Debug.Log(myBoolOne);
+        Debug.Log(myBoolTwo);
+        Debug.Log(myBoolThree);
+
+        for (int i = 0; i < myFloatArray.Length; i++)
+        {
+            Debug.Log(myFloatArray[i]);
+        }
+    }
 }
